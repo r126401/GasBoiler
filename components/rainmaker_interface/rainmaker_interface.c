ca55a7c5d284509fc46980c5467660001786c3bd
jajica <r126401@gmail.com>
2026-01-09 11:29:04 +0100

    Se incorpora rmaker_interface para la integracion con rainmaker

---
+++ components/rainmaker_interface/rainmaker_interface.c
@@ -0,0 +1,59 @@
+
+
+#include "rainmaker_interface.h"
+#include <string.h>
+#include <inttypes.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <esp_log.h>
+#include <esp_event.h>
+#include <nvs_flash.h>
+
+
+#include "esp_rmaker_common_events.h"
+
+#include "esp_rmaker_core.h"
+#include "esp_rmaker_console.h"
+#include "app_network.h"
+
+static const char *TAG = "rainmaker_interface.c";
+
+esp_rmaker_device_t *thermostat_device;
+
+
+
+void init_rainmaker_environment() {
+
+
+    /* Initialize Application specific hardware drivers and
+     * set initial state.
+     */
+    //app_driver_init();
+
+    /* Initialize NVS. */
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( err );
+
+    /* Initialize Wi-Fi. Note that, this should be called before esp_rmaker_node_init()
+     */
+    app_network_init();
+    
+    /* Initialize the ESP RainMaker Agent.
+     * Note that this should be called after app_network_init() but before app_network_start()
+     * */
+    esp_rmaker_config_t rainmaker_cfg = {
+        .enable_time_sync = false,
+    };
+    esp_rmaker_node_t *node = esp_rmaker_node_init(&rainmaker_cfg, "ESP RainMaker Device", "Temperature Sensor");
+    if (!node) {
+        ESP_LOGE(TAG, "Could not initialise node. Aborting!!!");
+        vTaskDelay(5000/portTICK_PERIOD_MS);
+        abort();
+    }
+
+
+}
