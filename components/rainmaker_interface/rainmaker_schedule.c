97a6ef9fc7f56a328202333c8c6185467e981868
r126401 <r126401@gmail.com>
2026-01-23 12:44:44 +0100

    Se incorpora parcialmente el schedule en el display. Falta que el termostato anote el setpoint_temperature y actuel el rele en funcion de ello

--- components/rainmaker_interface/rainmaker_schedule.c
+++ components/rainmaker_interface/rainmaker_schedule.c
@@ -96,7 +96,41 @@ static float extract_setpoint_temperature(cJSON *item) {
         return ESP_FAIL;
     }
 
+    ESP_LOGI(TAG, "Temperatura extraida del trigger: %.1f", cJSON_GetNumberValue(obj));
     return cJSON_GetNumberValue(obj);
+
+
+}
+
+static uint8_t extract_trigger_data(cJSON *trigger, int *min_of_trigger, int *mask_of_trigger) {
+
+    cJSON *index = NULL;
+    cJSON *obj = NULL;
+    int n_schedules = 0;
+
+
+    // Obtenemos los datos del trigger
+    int j, tam;
+    tam = cJSON_GetArraySize(trigger);
+    for (j=0;j<tam;j++) {
+        index = cJSON_GetArrayItem(trigger, j);
+        if ((obj = cJSON_GetObjectItem(index, "m")) == NULL) {
+            ESP_LOGE(TAG, "Error al extraer el parametro m: %s", cJSON_Print(index));
+            return ESP_FAIL;
+        }
+
+        *min_of_trigger = cJSON_GetNumberValue(obj);
+
+        if ((obj = cJSON_GetObjectItem(index, "d")) == NULL) {
+            ESP_LOGE(TAG, "Error al extraer el parametro d: %s", cJSON_Print(index));
+            return ESP_FAIL;
+        }
+
+        *mask_of_trigger = cJSON_GetNumberValue(obj);
+    }
+
+    return n_schedules;
+
 
 
 }
@@ -106,20 +140,20 @@ uint8_t get_next_schedule(int *min_of_day, int *min_of_trigger, float *setpoint_
 
     char *param_list;
     cJSON *json;
-    cJSON *schedule = NULL;
+    cJSON *programacion = NULL;
     cJSON *schedules = NULL;
-    cJSON *item;
+    cJSON *item_schedule;
     cJSON *trigger;
     cJSON *obj;
     cJSON *index;
     esp_err_t error =  ESP_OK;
-    uint8_t n_triggers;
+    uint8_t n_schedules;
     uint8_t i;
-    uint32_t candidate = -1;
-    uint32_t prev_candidate = -1;
+    int candidate = -1;
+    int prev_candidate = -1;
     time_t now;
 	struct tm fecha;
-    uint32_t mask_of_trigger = 0;
+    int mask_of_trigger = 0;
     uint8_t triggers = 0;
     float prev_setpoint = -1;
 
@@ -134,36 +168,41 @@ uint8_t get_next_schedule(int *min_of_day, int *min_of_trigger, float *setpoint_
         return ESP_FAIL;
     }
 
-    if ((schedule = cJSON_GetObjectItem(json, "schedule")) == NULL) {
+    if ((programacion = cJSON_GetObjectItem(json, "schedule")) == NULL) {
 
         ESP_LOGW(TAG, "No hay schedules");
         return ESP_FAIL;
 
     }
 
-    if ((schedules = cJSON_GetObjectItem(schedule, "schedules")) == NULL) {
+    if ((schedules = cJSON_GetObjectItem(programacion, "schedules")) == NULL) {
 
         ESP_LOGE(TAG, "Habia etiqueta pero no hay schedules");
         return ESP_FAIL;
     }
 
-    n_triggers = cJSON_GetArraySize(schedules);
-    for (i=0;i<n_triggers;i++) {
+    
+    n_schedules = cJSON_GetArraySize(schedules);
+    //Bucle para recorrer todos los schedules
+    for (i=0;i<n_schedules;i++) {
         ESP_LOGE(TAG, "Bucle %d", i);
-        item = cJSON_GetArrayItem(schedules, i);
+        item_schedule = cJSON_GetArrayItem(schedules, i);
 
         //Comprobamos si el schedule esta habilitado
-        if (cJSON_IsTrue(cJSON_GetObjectItem(item, "enabled")) == false) {
-            ESP_LOGE(TAG, "Este schedule esta inhibido %s", cJSON_Print(item));
+        if (cJSON_IsTrue(cJSON_GetObjectItem(item_schedule, "enabled")) == false) {
+            ESP_LOGE(TAG, "Este schedule esta inhibido %s", cJSON_Print(item_schedule));
             continue;
         }
 
-        if ((trigger = cJSON_GetObjectItem(item, "triggers")) == NULL) {
+        if ((trigger = cJSON_GetObjectItem(item_schedule, "triggers")) == NULL) {
 
             ESP_LOGE(TAG, "Error al extraer el trigger");
             return ESP_FAIL;
         }
 
+        extract_trigger_data(trigger, min_of_trigger, &mask_of_trigger);
+/*
+        // Obtenemos los datos del trigger
         int j, tam;
         tam = cJSON_GetArraySize(trigger);
         for (j=0;j<tam;j++) {
@@ -182,7 +221,7 @@ uint8_t get_next_schedule(int *min_of_day, int *min_of_trigger, float *setpoint_
 
             mask_of_trigger = cJSON_GetNumberValue(obj);
         }
-
+*/
         //Chequeamos si la mascara de la semana coincide.
         time(&now);
         localtime_r(&now, &fecha);
@@ -193,19 +232,20 @@ uint8_t get_next_schedule(int *min_of_day, int *min_of_trigger, float *setpoint_
         }
 
         *min_of_day = fecha.tm_hour * 60 + fecha.tm_min;
+        ESP_LOGI(TAG, "min_of_day: %d, min_of_trigger: %d", min_of_day, min_of_trigger);
         triggers++;
         if (*min_of_trigger <= *min_of_day) {
             ESP_LOGW(TAG, "trigger anterior a la hora actual");
             if (prev_candidate == -1) {
                 ESP_LOGW(TAG, "Primer schedule previo");
                 prev_candidate = *min_of_trigger;
-                prev_setpoint = extract_setpoint_temperature(item);
+                prev_setpoint = extract_setpoint_temperature(item_schedule);
                 ESP_LOGW(TAG, "Primer schedule previo. prev_setpoint: %.1f", prev_setpoint);
             } else {
                 if (prev_candidate > *min_of_trigger) {
                     ESP_LOGW(TAG, "Nuevo candidato para el prev_schedule");
                     prev_candidate = *min_of_trigger;
-                    prev_setpoint = extract_setpoint_temperature(item);
+                    prev_setpoint = extract_setpoint_temperature(item_schedule);
                 } else {
                     ESP_LOGW(TAG, "Seguimos iterando para el prev");
                 }
@@ -217,12 +257,12 @@ uint8_t get_next_schedule(int *min_of_day, int *min_of_trigger, float *setpoint_
             ESP_LOGW(TAG, "trigger posterior a la hora actual");
             if (candidate == -1 ) {
                 candidate = *min_of_trigger;
-                *setpoint_temperature = extract_setpoint_temperature(item);
+                *setpoint_temperature = extract_setpoint_temperature(item_schedule);
             } else {
                 if (candidate < *min_of_trigger) {
                     ESP_LOGW(TAG, "Nuevo candidato con indice %d", i);
                     candidate = *min_of_trigger;
-                    *setpoint_temperature = extract_setpoint_temperature(item);
+                    *setpoint_temperature = extract_setpoint_temperature(item_schedule);
 
                 } else {
                     ESP_LOGW(TAG, "Seguimos iterando");
